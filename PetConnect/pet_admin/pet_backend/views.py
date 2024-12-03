0f56874e67bffe26205ab7ec0013f0837bf123e4
Maycon-Martinelli <118576322+Maycon-Martinelli@users.noreply.github.com>
2024-12-02 21:37:16 -0400

    Add files via upload

--- PetConnect/pet_admin/pet_backend/views.py
+++ PetConnect/pet_admin/pet_backend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request): 
     return render(request, 'html/home.html', {'logo': 'image/petconnect.svg'})
 
@@ -9,4 +10,8 @@ def login(request):
 
 def cadastro(request):
     return render(request, 'html/cadastro.html', {'logo': 'image/petconnct.svg'})
+
+
+
+
 # Create your views here.
